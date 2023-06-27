267f43d9bcbea9d28a616a7195255a4465ba5125
HyeonE <mag30102000@gmail.com>
2023-06-28 00:26:08 +0900

    동적 라우팅

--- week-two/src/shared/Router.js
+++ week-two/src/shared/Router.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import About from "../pages/About";
 import Contact from "../pages/Contact";
 import Works from "../pages/Works";
-import About from "../pages/About";
+import Layout from "./Layout";
+import Work from "../pages/Work";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="about" element={<About />}></Route>
-        <Route path="contact" element={<Contact />}></Route>
-        <Route path="works" element={<Works />}></Route>
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="works" element={<Works />} />
+          <Route path="works/:id" element={<Work />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
